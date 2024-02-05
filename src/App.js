3b406dea952e44d43b1c2ef1989ce4e4f6b3fad0
Ranvir <ranvir.kumar@ongraph.com>
2024-02-05 15:10:18 +0530

    deeplink url

--- src/App.js
+++ src/App.js
@@ -6,49 +6,7 @@ function App() {
     const url = `infinityheadstart://${base}?data=${encodedData}`;
     return url;
   };
-  // const dataSet = (url) => {
-  //   return {
-  //     pdf: {
-  //       data: {
-  //         state: {
-  //           documentUrl: url,
-  //         },
-  //       },
-  //       pathName: "DocumentScreen",
-  //     },
-  //     video: {
-  //       data: {
-  //         state: {
-  //           activeVideo: {
-  //             url: url,
-  //           },
-  //         },
-  //       },
-  //       pathName: "PLAYLIST",
-  //     },
-  //     url: {
-  //       data: {
-  //         state: {
-  //           uri: url,
-  //         },
-  //       },
-  //       pathName: "WebStoryScreen",
-  //     },
-  //   };
-  // };
-  // const handleClick = (type, url = "") => {
-  // const { data, pathName } = dataSet(url)?.[type] || "";
-  // const deepLinkUrl = handleEncorderValue(data, pathName);
-  // // Attempt to open the deep link
-  // const testLink = document.createElement("a");
-  // testLink.href = deepLinkUrl;
-  // document.body.appendChild(testLink);
-  // testLink.click();
-  // document.body.removeChild(testLink);
-  // setTimeout(() => {
-  //   window.location.href = "https://play.google.com/store/apps";
-  // }, 1000);
-  // };
+  
   function getSearchParamsObject(url) {
     const searchParams = new URLSearchParams(url.search);
     const paramsObject = {};
@@ -84,36 +42,6 @@ function App() {
         margin: "10px",
       }}
     >
-      {/* <button
-        onClick={() =>
-          handleClick(
-            "pdf",
-            "https://www.africau.edu/images/default/sample.pdf"
-          )
-        }
-      >
-        open Deep link url for pdf
-      </button>
-      <button
-        onClick={() =>
-          handleClick(
-            "video",
-            "https://sample-videos.com/video321/mp4/720/big_buck_bunny_720p_10mb.mp4"
-          )
-        }
-      >
-        open Deep link url for video
-      </button>
-      <button
-        onClick={() =>
-          handleClick(
-            "url",
-            "https://storage.googleapis.com/gtv-videos-bucket/sample/images/ForBiggerBlazes.jpg"
-          )
-        }
-      >
-        open Deep link url for image
-      </button> */}
       <button onClick={() => handleDeepLink()}>open vedio deepLink Url</button>
     </div>
   );
